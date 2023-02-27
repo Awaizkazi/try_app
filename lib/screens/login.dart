172855415f033de403d83e3eba3855b7b50e7863
Awaizkazi <Owaizkazi789@gmail.com>
2023-02-27 12:33:47 +0530

    C

---
+++ lib/screens/login.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('assets/images/login.png'),
+        ),
+      ),
+    );
+  }
+}
