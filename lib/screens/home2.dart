ddc8dfec9a0735c912991eaac0ddbabc68bf0d84
Awaizkazi <Owaizkazi789@gmail.com>
2023-02-28 12:35:19 +0530

    Home2 Done

--- lib/screens/home2.dart
+++ lib/screens/home2.dart
@@ -10,38 +10,92 @@ class HomeScreen2 extends StatefulWidget {
 class _HomeScreen2State extends State<HomeScreen2> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      extendBodyBehindAppBar: true,
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-        actions: [
-          IconButton(
-            icon: const Icon(
-              Icons.favorite_outline,
-              color: Colors.black,
+    return SafeArea(
+      child: Scaffold(
+        extendBodyBehindAppBar: true,
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          title: Image.asset('assets/images/mey_logo.png'),
+          actions: [
+            IconButton(
+              icon: const Icon(
+                Icons.favorite_outline,
+                color: Colors.black,
+              ),
+              tooltip: 'Your favorites',
+              onPressed: () {},
             ),
-            tooltip: 'Your favorites',
-            onPressed: () {},
-          ),
-          IconButton(
-            icon: const Icon(
-              Icons.shopping_bag_outlined,
-              color: Colors.black,
+            IconButton(
+              icon: const Icon(
+                Icons.shopping_bag_outlined,
+                color: Colors.black,
+              ),
+              tooltip: 'Open shopping cart',
+              onPressed: () {},
             ),
-            tooltip: 'Open shopping cart',
-            onPressed: () {},
-          ),
-          IconButton(
-            icon: const Icon(
-              Icons.sort_outlined,
-              color: Colors.black,
+            IconButton(
+              icon: const Icon(
+                Icons.sort_outlined,
+                color: Colors.black,
+              ),
+              tooltip: 'Search by Filter',
+              onPressed: () {},
             ),
-            tooltip: 'Search by Filter',
-            onPressed: () {},
+          ],
+        ),
+        body: SingleChildScrollView(
+          child: Container(
+            padding: const EdgeInsets.symmetric(vertical: 60),
+            child: Column(
+              children: [
+                CustomContainerForImges(
+                  pathImage: 'assets/images/banner1.png',
+                ),
+                CustomContainerForImges(
+                  pathImage: 'assets/images/scroll_home_1.png',
+                ),
+                CustomContainerForImges(
+                  pathImage: 'assets/images/banner2.png',
+                ),
+                CustomContainerForImges(
+                  pathImage: 'assets/images/banner3.png',
+                ),
+                CustomContainerForImges(
+                  pathImage: 'assets/images/banner4.png',
+                ),
+                CustomContainerForImges(
+                  pathImage: 'assets/images/banner5.png',
+                ),
+              ],
+            ),
           ),
-        ],
+        ),
       ),
+    );
+  }
+}
+
+class CustomContainerForImges extends StatelessWidget {
+  final String? pathImage;
+
+  const CustomContainerForImges({
+    super.key,
+    this.pathImage,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SizedBox(
+          child: Image.asset(
+            pathImage!,
+            fit: BoxFit.cover,
+          ),
+          width: MediaQuery.of(context).size.width,
+        ),
+      ],
     );
   }
 }
