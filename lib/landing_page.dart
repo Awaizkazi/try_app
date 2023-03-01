8ca737c33d75760c37c6064ac095c1cf44bbf864
Awaizkazi <Owaizkazi789@gmail.com>
2023-03-01 19:05:19 +0530

    Account Screen In Progress

--- lib/landing_page.dart
+++ lib/landing_page.dart
@@ -8,6 +8,8 @@ class Profile_Screen extends StatefulWidget {
 }
 
 class _Profile_ScreenState extends State<Profile_Screen> {
+  // TODO VARIABLE FOR Static Color
+  final Color primaryBlueColor = Colors.blue;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -34,47 +36,114 @@ class _Profile_ScreenState extends State<Profile_Screen> {
         ],
       ),
       body: SingleChildScrollView(
-        child: Center(
+        child: Container(
+          margin: EdgeInsets.symmetric(vertical: 10),
+          padding: EdgeInsets.all(10),
           child: Column(
             children: [
               CircleAvatar(
-                radius: 50,
-                backgroundImage: NetworkImage(
-                    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOxVz2U6ihsCFFFniOjh2UaBXxg3W_dkiOpQ&usqp=CAU'),
+                radius: 55,
+                backgroundColor: Colors.blue.shade300,
+                child: CircleAvatar(
+                  radius: 50,
+                  backgroundImage: NetworkImage(
+                      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOxVz2U6ihsCFFFniOjh2UaBXxg3W_dkiOpQ&usqp=CAU'),
+                ),
               ),
               SizedBox(height: 10),
               Text(
                 'Owaiz Kazi',
-                style: Theme.of(context).textTheme.headlineSmall,
+                style: Theme.of(context)
+                    .textTheme
+                    .headlineSmall!
+                    .copyWith(fontWeight: FontWeight.bold),
               ),
               Text(
                 'Owaizkazi78@gmail.com',
                 style: Theme.of(context).textTheme.headlineSmall,
               ),
-              SizedBox(height: 10),
+              SizedBox(height: 20),
               SizedBox(
-                width: 200,
+                height: 50,
+                width: 250,
                 child: ElevatedButton.icon(
                   onPressed: () {},
                   icon: Icon(
                     Icons.edit,
-                    color: Colors.black,
+                    color: Colors.white,
                   ),
                   label: Text(
                     'Edit Profile',
-                    style: TextStyle(color: Colors.black),
+                    style: TextStyle(color: Colors.white, fontSize: 18),
                   ),
                   style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.amber,
+                    backgroundColor: Colors.blue,
                     side: BorderSide.none,
                     shape: StadiumBorder(),
                   ),
                 ),
               ),
+              SizedBox(height: 10),
+              Divider(),
+              // MENU
             ],
           ),
         ),
       ),
+    );
+  }
+}
+
+class ProfileMenuWidget extends StatelessWidget {
+  const ProfileMenuWidget({
+    super.key,
+    required this.title,
+    required this.icon,
+    required this.onPress,
+    this.endIcon = true,
+    this.textColor,
+  });
+
+  final String title;
+  final IconData icon;
+  final VoidCallback onPress;
+  final bool endIcon;
+  final Color? textColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      onTap: onPress,
+      leading: Container(
+        width: 40,
+        height: 40,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(100),
+          color: Colors.blue.withOpacity(0.1),
+        ),
+        child: Icon(
+          icon,
+          color: Colors.blue,
+        ),
+      ),
+      title: Text(
+        title,
+        style: Theme.of(context).textTheme.bodySmall?.apply(color: textColor),
+      ),
+      trailing: endIcon
+          ? Container(
+              width: 40,
+              height: 40,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(100),
+                color: Colors.blue.withOpacity(0.1),
+              ),
+              child: IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.arrow_forward_ios),
+              ),
+            )
+          : null,
     );
   }
 }
