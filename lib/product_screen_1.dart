6ef9bdd6497b1b3e1f5cd6e698aff3eb542ccc30
Awaizkazi <Owaizkazi789@gmail.com>
2023-03-06 11:59:58 +0530

    In Progress

--- lib/product_screen_1.dart
+++ lib/product_screen_1.dart
@@ -1,1 +1,10 @@
-impM
+import 'package:flutter/material.dart';
+
+class ProductScreen1 extends StatelessWidget {
+  const ProductScreen1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
