cb1d556770a3d63b81e5e9174c07ae3ff6f9cf42
Awaizkazi <Owaizkazi789@gmail.com>
2023-03-15 17:42:58 +0530

    Staggered grid View Flutter

--- lib/screens/home_3_3.dart
+++ lib/screens/home_3_3.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 class home_3_3 extends StatelessWidget {
   const home_3_3({super.key});
@@ -37,52 +38,55 @@ class home_3_3 extends StatelessWidget {
           ),
         ],
       ),
-      body: ListView(
-        children: [
-          CustomContainer(
-            c_width: double.infinity,
-            img_path: 'assets/images/home3/womenbag.png',
-          ),
-          SizedBox(height: 7),
-          Row(
-            children: [
-              CustomContainer(
-                c_width: 50,
-                c_height: 300,
-                img_path: 'assets/images/home3/spring_collection.png',
-              ),
-              CustomContainer(
-                c_width: 50,
-                c_height: 300,
-                img_path: 'assets/images/home3/spring_collection.png',
-              ),
-            ],
-          ),
-        ],
+      body: GridView.builder(
+        gridDelegate:
+            SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
+        itemBuilder: (context, index) {
+          return buildImageCard(index);
+        },
+        itemCount: 40,
       ),
     );
   }
-}
 
-class CustomContainer extends StatelessWidget {
-  final double? c_width, c_height;
-  final String? img_path;
-  final Color? container_color;
-  const CustomContainer({
-    Key? key,
-    this.c_height,
-    this.img_path,
-    this.c_width,
-    this.container_color,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: MediaQuery.of(context).size.width,
-      height: c_height,
-      child: Image.asset(img_path!),
-      color: container_color,
+  Widget buildImageCard(int index) {
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: Container(
+        margin: EdgeInsets.all(8),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(8),
+          child: Image.network(
+            'https://source.unsplash.com/random?sig=$index',
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
     );
   }
+
+// class CustomContainer extends StatelessWidget {
+//   final double? c_width, c_height;
+//   final String? img_path;
+//   final Color? container_color;
+//   const CustomContainer({
+//     Key? key,
+//     this.c_height,
+//     this.img_path,
+//     this.c_width,
+//     this.container_color,
+//   }) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       width: MediaQuery.of(context).size.width,
+//       height: c_height,
+//       child: Image.asset(img_path!),
+//       color: container_color,
+//     );
+//   }
+// }
 }
